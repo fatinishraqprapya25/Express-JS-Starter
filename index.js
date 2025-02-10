5f178323bcf866e3874f9b3b6bc946137d8d1ce1
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-10 13:46:49 +0600

    index page changed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = require("./routes");
 const notFoundHandler = require("./error/notFoundHandler");
+const globalErrorHandler = require("./error/globalErrorHandler");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", router);
+app.use(globalErrorHandler);
 app.use(notFoundHandler);
 
 module.exports = app;
