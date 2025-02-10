80212be94b475c9933d99b11e07760ae2cdb0f88
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-10 14:29:51 +0600

    index route optimized successfully

--- modules/auth/auth.route.js
+++ modules/auth/auth.route.js
@@ -1,7 +1,7 @@
 const authFeatures = require("./auth.features");
 const authRouter = require("express").Router();
 
-authRouter.post("/auth", authFeatures.register);
+authRouter.post("/", authFeatures.register);
 
 
 module.exports = authRouter;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
+const authRouter = require("../modules/auth/auth.route");
 const router = require("express").Router();
 
 const routes = [
-    { path: "/" }
+    { path: "/auth", router: authRouter }
 ];
+
+routes.map(r => router.use(r.path, r.router));
 
 module.exports = router;
