31ee44ee0b282a7f9f0f6cec9199a8a6e0f2570c
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-11 12:58:38 +0600

    basic uploader function done

---
+++ utils/upload.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const multer = require("multer");
+
+const uploader = (folder, allowedFileTypes, maxSize) => {
+    // define storage
+    const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, `uploads/${folder}`);
+        },
+        filename: (req, file, cb) => {
+            cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+        }
+    });
+
+    // filtering files
+    const filefilter = (req, file, cb) => {
+        if (allowedFileTypes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error(`${allowedFileTypes.toString()} are allowed!`), false);
+        }
+    }
+
+    // upload function
+    const upload = multer({
+        storage,
+        limits: { fileSize: maxSize * 1024 * 1024 },
+        fileFilter
+    });
+
+}
+
+module.exports = uploader;
