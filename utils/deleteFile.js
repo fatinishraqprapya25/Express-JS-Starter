da9ecd5907bb24226f023f8883bb016f93c6022c
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-12 09:52:47 +0600

    delete file utility function done

---
+++ utils/deleteFile.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const fs = require("fs").promises;
+
+const deleteFile = async (filePath) => {
+    try {
+        const actualPath = path.join(__dirname, filePath);
+        await fs.unlink(actualPath);
+        return true;
+    } catch (err) {
+        console.log("Error Deleting File: ", err.message);
+        return false;
+    }
+}
+
+module.exports = deleteFile;
