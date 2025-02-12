e779bae055491735fa2b446b6993dbbd90ee8f1d
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-12 11:50:26 +0600

    server page optimnized

--- server.js
+++ server.js
@@ -4,28 +4,46 @@ const app = require("./index");
 
 let server;
 
+const handleShutdown = async (signal) => {
+    console.log(`💥 ${signal} received! Shutting down gracefully...`);
+    try {
+        if (server) {
+            await server.close();
+            console.log("Closed all connections.");
+        } else {
+            console.log("server is not initialized!");
+        }
+
+        await mongoose.disconnect();
+        console.log("Disconnected from database.");
+        process.exit(0);
+    } catch (err) {
+        console.error("Error during shutdown:", err);
+        process.exit(1);
+    }
+};
+
 const main = async () => {
     try {
         const { port, mongoConnectionString } = config;
         await mongoose.connect(mongoConnectionString);
         server = app.listen(port, () => {
             console.log(`🚀 Server listening at port ${port}`);
-        })
+        });
     } catch (err) {
-        console.log("❌ Error connecting to database:", err.message)
+        console.error("❌ Error connecting to database:", err.message);
+        process.exit(1);
     }
-}
+};
 
 main();
 
 process.on("uncaughtException", (err) => {
-    console.error(err.name, err.message);
-    console.error("💥 Uncaught Exception! Shutting down...");
-    process.exit(1);
+    console.error("❌ Uncaught Exception: ", err.message);
+    handleShutdown("Uncaught Exception");
 });
 
 process.on("unhandledRejection", (err) => {
-    console.log(err.name, err.message);
-    console.log("💥 Unhandled Rejection! Shutting down...");
-    process.exit(1);
+    console.error("❌ Unhandled Rejection: ", err.message);
+    handleShutdown("Unhandled Rejection");
 });
