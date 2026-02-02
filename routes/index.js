205261fc0a53043916650a6235cc591212f21c79
Prapya <dev.prapya@gmail.com>
2026-02-02 07:34:40 -0500

    comma added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const authRouter = require("../modules/auth/auth.route");
 const router = require("express").Router();
 
 const routes = [
-    { path: "/auth", router: authRouter }
+    { path: "/auth", router: authRouter },
 ];
 
 routes.map(r => router.use(r.path, r.router));
